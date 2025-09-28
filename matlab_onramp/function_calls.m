c4a5747e69ae8bf170f4cabdb8cdaab9c3f0bbd1
Meet Jain <meetjain538@gmail.com>
2025-09-28 10:43:40 +0530

    Create function_calls.m

---
+++ matlab_onramp/function_calls.m
@@ -0,0 +1,3 @@
+% Create the variables vMax and ivMax containing the maximum value of the v2 vector and the corresponding index value, respectively.
+[dr,dc] = size(data)    % rows and columns of the data array
+[vMax, ivMax] = max(v2)
