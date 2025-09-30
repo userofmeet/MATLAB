84bfa65fe8701bb50745124b4de6f672301f692a
Meet Jain <meetjain538@gmail.com>
2025-09-30 19:13:37 +0530

    Create track_occlusion.m

---
+++ computer_vision_onramp/track_occlusion.m
@@ -0,0 +1,39 @@
+load ACFdetector4tracking
+turtleVideo = VideoReader("turtle.avi");
+frame = readFrame(turtleVideo);
+% Make detections.
+[bbox,score] = detect(detector,frame);
+bbox = bbox(score>95,:);
+score = score(score>95);
+bbox = selectStrongestBbox(bbox,score, ...
+    NumStrongest=1);
+centroid = [bbox(1)+bbox(3)/2  bbox(2)+bbox(4)/2];
+frame = insertShape(frame,"filled-circle",[centroid 50],Color="green");
+imshow(frame) 
+% Initialize Kalman filter.
+motionModel = "ConstantVelocity";
+initialLoc = centroid;
+initialError = [1 25];
+motionNoise = [1 25];
+measurementNoise = 100;
+kf = configureKalmanFilter(motionModel,initialLoc,initialError,motionNoise,measurementNoise);
+
+while hasFrame(turtleVideo)
+    trackedLoc = predict(kf);
+
+    frame = readFrame(turtleVideo);
+    [bbox,score] = detect(detector,frame);
+    bbox = bbox(score>95,:);
+    score = score(score>95);
+
+    if ~isempty(bbox)
+        strongestBbox = selectStrongestBbox(bbox,score,NumStrongest=1);
+        centroid = [strongestBbox(1)+strongestBbox(3)/2  strongestBbox(2)+strongestBbox(4)/2];
+        frame = insertShape(frame,"filled-circle",[centroid 50],Color="green");
+        trackedLoc = correct(kf,centroid);
+    end
+
+    frame = insertShape(frame,"filled-circle",[trackedLoc 20],Color="red");
+    imshow(frame)
+    drawnow
+end
